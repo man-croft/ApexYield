495184187612a123470fb71fd4bce4366d6f5bc1
diiabblo <solsticeveil46@gmail.com>
2026-02-09 04:34:53 +0100

    feat: export responsive hooks from index
    - Export useResponsive hook
    - Export useMediaQuery hook
    - Centralize all hook exports

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,8 +1,10 @@
 export { useBridge } from './useBridge';
 export { useWithdraw } from './useWithdraw';
-export { useUSDCxBalance } from './useUSDCxBalance';
 export { useVaultData } from './useVaultData';
+export { useUSDCxBalance } from './useUSDCxBalance';
 export { useWalletPersistence } from './useWalletPersistence';
 export { useStacksWalletPersistence } from './useStacksWalletPersistence';
 export { useUserPreferences } from './useUserPreferences';
-export { useUSDCxBalance } from './useUSDCxBalance';
+export { useTransactionTracker } from './useTransactionTracker';
+export { useResponsive } from './useResponsive';
+export { useMediaQuery } from './useMediaQuery';
